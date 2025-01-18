313e8d70ca4272fa8b3656bb9efaac1f6f6c04f6
CCCplusplus <139177589+CCCplusplus@users.noreply.github.com>
2025-01-17 18:53:12 -0600

    Update main.c
    3D camera and cube for testing

--- src/main.c
+++ src/main.c
@@ -18,16 +18,25 @@ int main ()
 
 	// Create the window and OpenGL context
 	InitWindow(1280, 800, "Hello Raylib");
+	// ToggleFullscreen();
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
 	// Load a texture from the resources directory
 	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	
+
+	Camera3D camera = { 0 };
+	camera.position = (Vector3){ 4, 0, 2 };
+	camera.target = (Vector3){ 0, 0, 0 };
+	camera.up = (Vector3){ 0,1,0 };
+	camera.fovy = 45;
+	camera.projection = CAMERA_PERSPECTIVE;
+
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
+		UpdateCamera(&camera, CAMERA_FREE);
 		// drawing
 		BeginDrawing();
 
@@ -35,10 +44,18 @@ int main ()
 		ClearBackground(BLACK);
 
 		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
+		//DrawText("Hello Raylib", 200,200,20,WHITE);
 
 		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+		//DrawTexture(wabbit, 400, 200, WHITE);
+
+		BeginMode3D(camera);
+
+		DrawCube((Vector3) { 0, 0, 0 }, 5,5,5, RED);
+
+		EndMode3D();
+
+		
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
